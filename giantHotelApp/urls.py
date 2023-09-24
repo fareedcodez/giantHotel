40b2b232927c47e6aeffe306f50e33647a17241a
sittangwesa <sitangwesa6gmail.com>
2023-09-24 05:20:31 +0300

    pages

--- giantHotelApp/urls.py
+++ giantHotelApp/urls.py
@@ -3,5 +3,8 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name='home'),
-    
+    path('room-details/', views.roomDetails, name='room-details'),
+    path('rooms/', views.rooms, name='rooms'),
+    path('contact-us/', views.contactUs, name='contact-us'),
+    path('about-us/', views.aboutUs, name='about-us'),
 ]
--- giantHotelApp/views.py
+++ giantHotelApp/views.py
@@ -2,3 +2,15 @@ from django.shortcuts import render
 
 def home(request):
     return render (request, 'pages/index.html')
+
+def roomDetails(request):
+    return render (request, 'giantHotelApp/roomDetails.html')
+
+def rooms(request):
+    return render (request, 'giantHotelApp/rooms.html')
+
+def contactUs(request):
+    return render (request, 'pages/contactUs.html')
+
+def aboutUs(request):
+    return render (request, 'pages/aboutUs.html')

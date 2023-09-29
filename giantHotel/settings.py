8d9707e3d2ac07fc0559cb50e4e7e029e7e8dfc7
jovyinny <jovinemutelani@gmail.com>
2023-09-29 16:06:47 +0300

    Minor changes in setting.py --> changed static_root

--- giantHotel/settings.py
+++ giantHotel/settings.py
@@ -126,9 +126,10 @@ STATIC_URL = 'static/'
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static')
 ]
-STATIC_ROOT= os.path.join(BASE_DIR, 'assets')
+# STATIC_ROOT= os.path.join(BASE_DIR, 'assets')
 MEDIA_URL ='/img/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'static/img')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build', 'static')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field

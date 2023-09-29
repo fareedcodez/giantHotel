5202127d194c7a8218127d7c292197fb827b6e8e
jovyinny <jovinemutelani@gmail.com>
2023-09-29 16:45:00 +0300

    Minor change s

--- giantHotelApp/views.py
+++ giantHotelApp/views.py
@@ -12,5 +12,5 @@ def rooms(request,template_name='giantHotelApp/rooms.html'):
 def contactUs(request,template_name='pages/contactUs.html'):
     return render (request, template_name=template_name)
 
-def aboutUs(request,template_name='giantHotelApp/aboutUs.html'):
+def aboutUs(request,template_name='pages/aboutUs.html'):
     return render (request, template_name=template_name)
